6a985e99b02785fa10c559c1b8bacb086c532e46
devvime <dihapp@outlook.com>
2024-05-11 00:03:04 -0300

    add function uploadAll in fileService

--- src/FileService.php
+++ src/FileService.php
@@ -2,28 +2,29 @@
 
 namespace Devvime\Kiichi\Engine;
 
-class FileService {
+class FileService
+{
 
-    public function upload($img, $folder = UPLOAD_DIR)
+	public function upload($img, $folder = UPLOAD_DIR)
 	{
 		if (isset($img['name']) && $img["error"] == 0) {
-		    $file_temp = $img['tmp_name'];
-		    $name = $img['name'];
-		    $extension = strrchr($name, '.');
-		    $extension = strtolower($extension);
-		    if (strstr('.jpg;.jpeg;.gif;.png', $extension)) {
-		        $new_name = uniqid() . '-'.str_replace(" ","-",$name);
-		        $destiny = $folder . $new_name;
-		        move_uploaded_file( $file_temp, $destiny);
-		    }else {
-		        echo json_encode(["error"=>"Você poderá enviar apenas arquivos \'*.jpg;*.jpeg;*.gif;*.png\'"]);
-		        exit;
-		    }
-		}else {
-	        echo json_encode(["error"=>"Você não enviou nenhum arquivo!"]);
-	        exit;
-		} 
-		return $new_name;		
+			$file_temp = $img['tmp_name'];
+			$name = $img['name'];
+			$extension = strrchr($name, '.');
+			$extension = strtolower($extension);
+			if (strstr('.jpg;.jpeg;.gif;.png', $extension)) {
+				$new_name = uniqid() . '-' . str_replace(" ", "-", $name);
+				$destiny = $folder . $new_name;
+				move_uploaded_file($file_temp, $destiny);
+			} else {
+				echo json_encode(["error" => "Você poderá enviar apenas arquivos \'*.jpg;*.jpeg;*.gif;*.png\'"]);
+				exit;
+			}
+		} else {
+			echo json_encode(["error" => "Você não enviou nenhum arquivo!"]);
+			exit;
+		}
+		return $new_name;
 	}
 
 	public function uploadBase64($folder = UPLOAD_DIR)
@@ -39,4 +40,29 @@ class FileService {
 		return $file;
 	}
 
+	public function uploadAll($filesArray, $uploadDir)
+	{
+		$uploadedFiles = array();
+		if (!file_exists($uploadDir)) {
+			mkdir($uploadDir, 0777, true);
+		}
+		foreach ($filesArray['name'] as $key => $fileName) {
+			$fileTmpName = $filesArray['tmp_name'][$key];
+			$fileType = $filesArray['type'][$key];
+			$fileSize = $filesArray['size'][$key];
+			$fileError = $filesArray['error'][$key];
+			if ($fileError === UPLOAD_ERR_OK) {
+				$uniqueFileName = uniqid() . '_' . $fileName;
+				$uploadPath = $uploadDir . '/' . $uniqueFileName;
+				if (move_uploaded_file($fileTmpName, $uploadPath)) {
+					$uploadedFiles[] = $uniqueFileName;
+				} else {
+					$uploadedFiles[] = "Error moving file $fileName to upload directory.";
+				}
+			} else {
+				$uploadedFiles[] = "Error sending file $fileName. Error code: $fileError";
+			}
+		}
+		return $uploadedFiles;
+	}
 }
